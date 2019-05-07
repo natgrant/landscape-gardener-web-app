0ec9e67db6b7fb7523bdc1d93266e7954c8b46b9
Nat Grant <email.natgrant@gmail.com>
2019-05-07 15:00:51 +1200

    webpack config setup

---
+++ webpack.config.js
@@ -0,0 +1,26 @@
+const path = require("path");
+
+module.exports = {
+  entry: "./client/index.js",
+  output: {
+    path: path.join(__dirname, "/public"),
+    filename: "bundle.js"
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /node_modules/,
+        loader: "babel-loader"
+      },
+      {
+        test: /\.(scss|css)$/,
+        use: ["style-loader", "css-loader", "sass-loader"]
+      }
+    ]
+  },
+  resolve: {
+    extensions: [".js", ".jsx"]
+  },
+  devtool: "source-map"
+};
