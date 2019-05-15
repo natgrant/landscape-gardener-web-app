17774688e7d4c4e28deef225cafa171ba2558708
Nat Grant <email.natgrant@gmail.com>
2019-05-15 12:18:11 +1200

    updated to use hooks

--- client/components/Services.jsx
+++ client/components/Services.jsx
@@ -1,72 +1,34 @@
-import React, { Component } from "react";
-import { connect } from "react-redux";
+import React, { useState } from "react";
 
-import { fetchServices } from "../actions/index";
 import ServiceDescription from "./ServiceDescription";
 
-export class Services extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      showModal: false
-    };
-  }
-
-  // componentDidMount = () => {
-  //   this.props.fetchServices();
-  // };
-
-  handleChange = () => {
-    this.setState({
-      showModal: !this.state.showModal
-    });
-  };
-
-  render() {
-    console.log(this.props);
-    let miao = ["test", "test", "test"];
-    return (
-      <div className="services-container has-text-centered">
-        <h2 className="sub-header">Services</h2>
-        <div className="columns is-desktop">
-          {miao.map((service, i) => {
-            return (
-              <div key={i} className="column" onClick={this.handleChange}>
-                <ServiceDescription
-                  service={service}
-                  isShowing={this.state.showModal}
-                />
-                <figure className="is-inline-block service-img">
-                  <img
-                    className="img-services-circular"
-                    src="/images/lawn.jpg"
-                  />
-                  <h3 className="img-text">
-                    <span>{service}</span>
-                  </h3>
-                </figure>
-              </div>
-            );
-          })}
-        </div>
+export function Services() {
+  const [showModal, hideModal] = useState(false);
+  let miao = ["test", "test", "test"];
+  return (
+    <div className="services-container has-text-centered">
+      <h2 className="sub-header">Services</h2>
+      <div className="columns is-desktop">
+        {miao.map((service, i) => {
+          return (
+            <div
+              key={i}
+              className="column"
+              onClick={() => hideModal(!showModal)}
+            >
+              <ServiceDescription service={service} isShowing={showModal} />
+              <figure className="is-inline-block service-img">
+                <img className="img-services-circular" src="/images/lawn.jpg" />
+                <h3 className="img-text">
+                  <span>{service}</span>
+                </h3>
+              </figure>
+            </div>
+          );
+        })}
       </div>
-    );
-  }
+    </div>
+  );
 }
 
-const mapStateToProps = ({ services }) => {
-  return {
-    services
-  };
-};
-
-const mapDispatchToProps = dispatch => {
-  return {
-    fetchServices: () => dispatch(fetchServices())
-  };
-};
-
-export default connect(
-  mapStateToProps,
-  mapDispatchToProps
-)(Services);
+export default Services;
