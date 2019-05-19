7afc06f42c6c91c10a99b163e53c566321cb68dd
Nat Grant <email.natgrant@gmail.com>
2019-05-20 11:44:11 +1200

    modal wip

--- client/components/ServiceDescription.jsx
+++ client/components/ServiceDescription.jsx
@@ -4,21 +4,14 @@ const ServiceDescription = ({ isShowing, handleChange, service }) => {
   if (isShowing) {
     return (
       <Fragment>
-        <div className="modal is-active">
-          <div className="modal-background" />
-          <div className="modal-card">
-            <header className="modal-card-head">
-              <p className="modal-card-title">{service.title}</p>
-            </header>
-            <section className="modal-card-body">
-              <p>{service.description}</p>
-            </section>
-            <button
-              className="modal-close is-large"
-              aria-label="close"
-              onClick={handleChange}
-            />
-          </div>
+        <div className="modal-container">
+          <p className="modal-title">{service.title}</p>
+
+          <button
+            className="modal-close is-large"
+            aria-label="close"
+            onClick={handleChange}
+          />
         </div>
       </Fragment>
     );
