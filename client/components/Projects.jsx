c883284aeefd262e5afcdf5cf2934297aad03212
Nat Grant <email.natgrant@gmail.com>
2019-05-21 14:06:28 +1200

    layout for projects

--- client/components/Projects.jsx
+++ client/components/Projects.jsx
@@ -4,24 +4,42 @@ import useFetch from "../helpers/useFetch";
 
 export default function Projects() {
   const data = useFetch("/api/v1/gardens");
-  console.log(data);
   return (
     <div className="projects-container has-text-centered">
       <h2 className="sub-header">Projects</h2>
-      <div className="columns is-desktop">
-        {data.map((project, i) => {
+      {data.map((project, i) => {
+        if (project.id % 2 !== 0) {
           return (
-            <div key={i} className="column">
-              <figure className="is-inline-block service-img">
-                <img className="img-projects" src={project.image} />
-                <h3 className="">{project.name}</h3>
+            <div className="columns" key={i}>
+              <div id={`project-${project.id}`} className="column is-6">
+                <figure className="service-img">
+                  <img className="img-projects" src={project.image} />
+                </figure>
+              </div>
+              <div className="column is-6">
+                <h3>{project.name}</h3>
                 <p>{project.location}</p>
                 <p>{project.description}</p>
-              </figure>
+              </div>
             </div>
           );
-        })}
-      </div>
+        } else {
+          return (
+            <div className="columns" key={i}>
+              <div className="column is-6">
+                <h3>{project.name}</h3>
+                <p>{project.location}</p>
+                <p>{project.description}</p>
+              </div>
+              <div id={`project-${project.id}`} className="column is-6">
+                <figure className="service-img">
+                  <img className="img-projects" src={project.image} />
+                </figure>
+              </div>
+            </div>
+          );
+        }
+      })}
     </div>
   );
 }
