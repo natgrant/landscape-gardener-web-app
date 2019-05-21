eb2c7c667909c3ad8692750d1a438212519c573b
Nat Grant <email.natgrant@gmail.com>
2019-05-21 13:36:46 +1200

    project seed data displaying

--- client/components/Projects.jsx
+++ client/components/Projects.jsx
@@ -6,8 +6,22 @@ export default function Projects() {
   const data = useFetch("/api/v1/gardens");
   console.log(data);
   return (
-    <div>
-      <p>projects page</p>
+    <div className="projects-container has-text-centered">
+      <h2 className="sub-header">Projects</h2>
+      <div className="columns is-desktop">
+        {data.map((project, i) => {
+          return (
+            <div key={i} className="column">
+              <figure className="is-inline-block service-img">
+                <img className="img-projects" src={project.image} />
+                <h3 className="">{project.name}</h3>
+                <p>{project.location}</p>
+                <p>{project.description}</p>
+              </figure>
+            </div>
+          );
+        })}
+      </div>
     </div>
   );
 }
--- seeds/portfolio.js
+++ seeds/portfolio.js
@@ -7,22 +7,25 @@ exports.seed = function(knex, Promise) {
           id: 1,
           name: "Deck and screen",
           location: "Brooklyn",
-          description: "Something about the project goes here",
-          image: ""
+          description:
+            "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Pellentesque sit amet porttitor eget dolor morbi. Neque viverra justo nec ultrices.",
+          image: "/images/placeholder1.jpg"
         },
         {
           id: 2,
           name: "Flower Bed",
           location: "Lower Hutt",
-          description: "Something about the project goes here",
-          image: ""
+          description:
+            "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Pellentesque sit amet porttitor eget dolor morbi. Neque viverra justo nec ultrices.",
+          image: "/images/placeholder2.jpg"
         },
         {
           id: 3,
           name: "Paving of Courtyard",
           location: "Eastbourne",
-          description: "Something about the project goes here",
-          image: ""
+          description:
+            "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Pellentesque sit amet porttitor eget dolor morbi. Neque viverra justo nec ultrices.",
+          image: "/images/placeholder3.jpg"
         }
       ]);
     });
