261458398d83b362a660beed3eeb289acfe5a32e
Nat Grant <email.natgrant@gmail.com>
2019-05-21 14:36:48 +1200

    contact page wip

--- client/containers/ContactPage.jsx
+++ client/containers/ContactPage.jsx
@@ -6,17 +6,12 @@ import ContactDetails from "../components/ContactDetails";
 export class Contact extends Component {
   render() {
     return (
-      <div>
-        <section className="hero is-medium is-primary is-bold">
-          <div className="hero-body">
-            <div className="container">
-              <h1 className="title">Get in touch</h1>
-              <h2 className="subtitle">Please enquire below</h2>
-            </div>
-          </div>
-        </section>
-        <div className="columns contact-info">
-          <ContactDetails />
+      <div className="columns margin-0">
+        <div className="column contact-title is-6">
+          <h1 className="title">Contact Us</h1>
+        </div>
+
+        <div className="column is-6 contact-info">
           <ContactForm />
         </div>
       </div>
