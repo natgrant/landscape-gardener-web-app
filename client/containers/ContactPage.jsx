fd0b628ec59efbd5c5017b27acbf229791a2850f
Nat Grant <email.natgrant@gmail.com>
2019-07-19 12:20:56 +1200

    minor style change

--- client/containers/ContactPage.jsx
+++ client/containers/ContactPage.jsx
@@ -7,7 +7,7 @@ export class Contact extends Component {
     return (
       <div className="columns contact-col">
         <div className="column contact-title is-4" />
-        <div className="column is-6 contact-info">
+        <div className="column is-6 contact-info form-margin">
           <HooksForm />
         </div>
       </div>
